ab405e0037de33eeff0f327985a703a16a4e0ffe
Murilo Gomes <murilolbgomes@gmail.com>
2020-07-06 09:56:54 -0300

    arquivos

---
+++ Backend/ProjetoEntrevista/ProjetoEntrevista/Domains/UsuarioPFDomains.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoEntrevista.Domains
+{
+    public class UsuarioPFDomains
+    {
+        public int IdUsuarioPf { get; set; }
+        public string NomeUsuario { get; set; }
+        public int IdTipo { get; set; }
+        public int NumeroCpf { get; set; }
+        public int Telefone { get; set; }
+    }
+}
